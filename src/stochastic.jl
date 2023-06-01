5745d1cadc75422201a04e939ebcaab5df206ebb
francescocaredda <francecaredda6@gmail.com>
2023-06-01 17:19:57 +0200

    wip

--- src/stochastic.jl
+++ src/stochastic.jl
@@ -222,6 +222,7 @@ function artrainer(D,η,batch_size,n_epoch;
 
     savefile !== nothing && close(file)
     p0 = computep0(D)
+    
     arnet = arnet_builder(m,p0)
 
     return arnet, m
@@ -408,5 +409,91 @@ function arnet_builder2(m, p0; q = 21)
     return net
 end
 
+function artrainer(D::Tuple{Matrix{Int}, Vector{Float64}}, η::Float64, batch_size::Int , n_epochs::Int, idxperm::Vector{Int}; 
+    init_m = Nothing,  
+    H = 32,
+    d = 23, 
+    init = rand,
+    λ=0.001, 
+    structfile = "../ArDCAData/data/PF00014/PF00014_struct.dat",
+    savefile::Union{String, Nothing} = nothing)
+    
 
 
+    N,_ = size(D[1])
+    q = maximum(D[1])
+
+    m = if init_m !== Nothing
+        init_m
+    else
+        (Q = init(H,d,N), K = init(H,d,N), V = init(H,q,q))
+    end
+    
+    t = setup(Adam(η), m)
+
+    savefile !== nothing && (file = open(savefile,"a"))
+    
+    for i in 1:n_epochs
+        loader = DataLoader(D, batchsize = batch_size, shuffle = true)
+        for (z,w) in loader
+            _w = w/sum(w)
+            g = gradient(x->arloss(x.Q, x.K, x.V, z, _w, λ=λ),m)[1];
+            update!(t,m,g)
+        end
+
+        #s = score(m.Q,m.K,m.V);
+        #PPV = compute_PPV(s,structfile)
+        l = round(arloss(m.Q, m.K, m.V, D[1], D[2], λ=λ),digits=5) 
+        #p = round((PPV[N]),digits=3)
+        #println("Epoch $i loss = $l \t PPV@L = $p \t First Error = $(findfirst(x->x!=1, PPV))")
+        println("Epoch $i loss = $l")
+        savefile !== nothing && println(file, "Epoch $i loss = $l \t PPV@L = $p \t First Error = $(findfirst(x->x!=1, PPV))")
+    end
+
+    savefile !== nothing && close(file)
+    p0 = computep0(D)
+    
+    @tullio W[h, i, j] := m.Q[h,d,i]*m.K[h,d,j] - 1.0e8*(j>=i)
+    W = softmax(W,dims=3)
+    @tullio J[i,j,a,b] := W[h,i,j]*m.V[h,a,b]*(i!=1)
+    J_reshaped = AttentionBasedPlmDCA.reshapetensor(J,N,q)
+    F = [zeros(q) for _ in 1:N-1]
+    arnet = ArNet(idxperm, p0, J_reshaped,F)
+
+    return arnet, m
+end
+
+
+function artrainer(filename::String, η::Float64, batch_size::Int, n_epochs::Int, permorder::Union{Symbol, Vector{Int}};
+    theta::Union{Symbol,Real}=:auto,
+    max_gap_fraction::Real=0.9,
+    remove_dups::Bool=true,
+    kwds...)
+    
+    time = @elapsed Weights, Z, N, M, q = ReadFasta(filename, max_gap_fraction, theta, remove_dups)
+    
+    idxperm = if typeof(permorder) == Symbol
+        S = entropy(Z,Weights)
+        if permorder === :ENTROPIC
+            sortperm(S)
+        elseif permorder === :REV_ENTROPIC
+            sortperm(S,rev=true)
+        elseif permorder === :RANDOM
+            randperm(N)
+        elseif permorder === :NATURAL
+            collect(1:N)
+        else
+            error("the end of the world has come")
+        end
+    elseif typeof(permorder) <: Vector
+        (length(permorder) != N) && error("length permorder ≠ $N")
+        isperm(permorder) && (permorder)
+    else
+        error("permorder can only be a Symbol or a Vector")
+    end
+
+    ArDCA.permuterow!(Z,idxperm) 
+    data = (Z,Weights)
+    println("preprocessing took $time seconds")
+    artrainer(data, η, batch_size, n_epochs, idxperm; kwds...)
+end
