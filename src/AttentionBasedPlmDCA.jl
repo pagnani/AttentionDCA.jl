ab50fe787a3fd5607103c1f00e3700134fe5b243
francescocaredda <francecaredda6@gmail.com>
2023-06-09 12:34:06 +0200

    wip

--- src/AttentionBasedPlmDCA.jl
+++ src/AttentionBasedPlmDCA.jl
@@ -14,6 +14,7 @@ import Distributions: wsample
 using ExtractMacro
 using ArDCA
 
+
 export AttOut, AttPlmVar, FieldAttPlmVar, attentiondca, arattentiondca, score, compute_freq, compute_PPV, L2reg, sample, my_attentiondca, trainer, artrainer, artrainer2
 
 include("types.jl")
